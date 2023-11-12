864593ada977223b65b68c8edde95ca6d4543775
Amit-Bongale <amitbongale14@gmail.com>
2023-11-12 17:25:04 +0530

    nav-link items added

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -1,13 +1,29 @@
 import { Outlet, Link } from "react-router-dom";
 
-function Nav(){
-    return(
-        <div>
-            <div>logo</div>
-            <div>
-                home category cars 
-            </div>
+import './css/nav.css'
 
+
+function Nav(){
+
+    return(
+        
+        <div>
+
+            <div className="nav-container flex">
+
+                <div className="logo">logo</div>
+
+                <div className=" nav-elemts flex-center">
+
+                    <button className="nav-link">Home</button>
+                    <button className="nav-link">Cars</button>
+                    <button className="nav-link">Login</button>
+                    <button className="nav-link">Contact us</button>
+
+                </div>
+
+            </div>
+            
         </div>
     );
 }

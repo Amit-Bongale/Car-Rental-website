74287b6ecdc7ca6b5300a0ad6d89f21e2661b570
puneeth037h <punithshanakanahalli@gmail.com>
2023-11-22 15:36:00 +0530

    hi

--- src/Components/Routers.js
+++ src/Components/Routers.js
@@ -2,7 +2,7 @@ import {Routes, Route } from 'react-router-dom';
 
 import Home from './Pages/Home';
 import Login from './Pages/Login';
-import Cardetails from './Pages/CarDetails';
+import Cardetails from './Pages/Cardetails';
 
 function CustomRouters(){
     return(
@@ -10,7 +10,7 @@ function CustomRouters(){
         <Routes>
             <Route path='/' element={<Home></Home>}> </Route>
             <Route path='/login' element={<Login></Login>} ></Route>
-            <Route path='/cardetails/:carname?' element={<Cardetails></Cardetails>}></Route>
+            <Route path='/cardetails' element={<Cardetails></Cardetails>}></Route>
         </Routes>
     );
     

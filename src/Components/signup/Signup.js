13234d5572797ce46aec7f6f4471b8b2f934fddc
Amit-Bongale <amitbongale14@gmail.com>
2023-12-02 12:46:07 +0530

    sign up design changed

--- src/Components/signup/Signup.js
+++ src/Components/signup/Signup.js
@@ -7,57 +7,50 @@ import { Link } from 'react-router-dom';
 function Signup(){
 
     return(
+        
+        <div className="main-container-sigin">
+            <div className="Signin-main-container">
 
-        <div>
-            <div className="main-container-sigin">
+                <div className="Main-signin-form">
 
-                <div className="Signin-main-container">
-
-                    <div className="signin-side-img">
-                        <div className="signin-Quote-container">
-                            {/* <h2>Beyond Destinations, Create Connections</h2> */}
-                            <h2>Don't just Dream it, <br/> Rent it! </h2>
-                        </div>
+                    <div className="signin-logo">
+                        <h2>Welcome to Rides for Rent</h2>
                     </div>
 
-                    <div className="Main-signin-form">
-
-                        <div className="signin-logo">
-                            <h2>Welcome to Rides for Rent</h2>
+                    <div className="Signin_form">
+                        
+                        <div className="Username-container">
+                            <label>Username</label>
+                            <input type="text" />
                         </div>
 
-                        <div className="Signin_form">
+                        <div className="Username-container">
+                            <label>Email</label>
+                            <input type="text" />
+                        </div>   
+
+                        <div className="Username-container">
+                            <label>Mobile Number</label>
+                            <input type="number" />
+                        </div>
+
+                        <div className="Username-container">
+                            <label>password</label>
+                            <input type="text" />
+                        </div> 
+                        
+                        <div className="Signin_button_container">
+                            <button className="Signin_button">Sign up</button>
+                        </div>
+
+                        <div className="Signup_button_container">
+                            Alredy our Customer?
+                            <Link to='/login'>
+                            <button className='card-button' id='login-button'>
+                                <span>login</span>
+                                <img src={arrow} alt="arrow" />
+                            </button> </Link>
                             
-                            <div className="Username-container">
-                                <label>Username</label>
-                                <input type="text" />
-                            </div>
-                            <div className="Username-container">
-                                <label>Email</label>
-                                <input type="text" />
-                            </div>   
-                            <div className="Username-container">
-                                <label>Number</label>
-                                <input type="password" />
-                            </div>
-                            <div className="Username-container">
-                                <label>password</label>
-                                <input type="text" />
-                            </div> 
-                            <div className="Signin_button_container">
-                                <button className="Signin_button">Sign in</button>
-                            </div>
-
-                            <div className="Signup_button_container">
-                                Alredy our Customer?
-                                <Link to='/login'>
-                                <button className='card-button' id='login-button'>
-                                    <span>login</span>
-                                    <img src={arrow} alt="arrow" />
-                                </button> </Link>
-                                
-                            </div>
-
                         </div>
 
                     </div>

fd95ff93c5ad558910b73d023ffc551ec2f52fcf
Amit-Bongale <amitbongale14@gmail.com>
2023-12-10 22:06:22 +0530

    admin delete section created

--- src/Components/Admin/DeleteCars/Deletecars.js
+++ src/Components/Admin/DeleteCars/Deletecars.js
@@ -1,40 +1,105 @@
 import React from 'react'
 import AdminNav from '../AdminNav/AdminNav';
 
+import { useState, useEffect } from 'react';
 import './deletecars.css'
 
 function Deletecars() {
+
+  let [carsdata , setcars] = useState([])
+
+  useEffect(() => {
+      try {
+          fetch('http://localhost:3000/carsdata', { method: "POST" })
+              .then((res) => res.json())
+              .then((data) => {
+                  console.log('Fetched data:', data);
+                  setcars(data);
+              })
+              .catch(error => {
+                  console.log('Error fetching data:', error);
+              });
+      } catch (error) {
+          console.error('Error:', error);
+      }
+  },[])
+
+
   return (
     <div> 
         <AdminNav></AdminNav>
 
         <div className='cars-details-container'>
-        <table className='details-table'>
-            <thead>
-                <th>Car Name</th>
-                <th>Driver Name</th>
-                <th>Driver Rating</th>
-                <th>car Rating</th>
-                <th>Seats</th>
-                <th>Pricing per km</th>
-                <th>Contact Number</th>
-                <th>Email</th>
-                <th>Image 1</th>
-                <th>Image 2</th>
-                <th>Image 3</th>
-                <th>Image 4</th>
-            </thead>
-            <tbody>
-                <tr>
-                <td >
-                   model
-                </td>
-                <td >
-                   car name
-                </td>
-                </tr>
-            </tbody>
-        </table>
+          
+            <table className='details-table'>
+
+              <tr>
+                <th className="details-table-header"> </th>
+                <th className="details-table-header"> Car Name</th>
+                <th className="details-table-header"> Driver Name</th>
+                <th className="details-table-header"> Driver Rating</th>
+                <th className="details-table-header"> car Rating</th>
+                <th className="details-table-header"> Seats</th>
+                <th className="details-table-header"> Pricing per km</th>
+                <th className="details-table-header"> Contact Number</th>
+                <th className="details-table-header"> Email</th>
+                <th className="details-table-header"> Image 1</th>
+                <th className="details-table-header"> Image 2</th>
+                <th className="details-table-header"> Image 3</th>
+                <th className="details-table-header"> Image 4</th>
+              </tr>
+                
+              {
+                carsdata.map((car) => (
+                  
+                  <tr className="deatils-table-row">
+                    <td className="details-table-data">
+                        <button> delete car</button>
+                    </td>
+                    <td className="details-table-data">
+                        {car.model} 
+                    </td>
+                    <td className="details-table-data">
+                        {car.drivername}
+                    </td>
+                    <td className="details-table-data">
+                        {car.driverrating}
+                    </td>
+                    <td className="details-table-data">
+                        {car.carrating}
+                    </td>
+
+                    <td className="details-table-data">
+                        {car.seats}
+                    </td>
+                    <td className="details-table-data">
+                        {car.priceperkm}
+                    </td>
+
+                    <td className="details-table-data">
+                        {car.drivercontact}
+                    </td>
+                    <td className="details-table-data">
+                        {car.driveremail}
+                    </td>
+                    <td className="details-table-data">
+                        <img  src={car.imageurl1} alt="image1" height={40} />
+                    </td>
+                    <td className="details-table-data">
+                            <img  src={car.imageurl2} alt="image1" height={40} />
+                    </td>
+                    <td className="details-table-data">
+                        <img  src={car.imageurl3} alt="image1" height={40} />
+                    </td>
+                    <td className="details-table-data">
+                        <img  src={car.imageurl4} alt="image1" height={40} />
+                    </td>
+                  </tr>
+                  
+                ))
+              }
+                
+            </table>
         </div>
 
     </div>
--- src/Components/Admin/Insercars/Insertcars.js
+++ src/Components/Admin/Insercars/Insertcars.js
@@ -1,135 +1,136 @@
-import AdminNav from "../AdminNav/AdminNav"
+import AdminNav from "../AdminNav/AdminNav";
 
-import './Insertcars.css'
+import "./Insertcars.css";
 
 function Insertcars() {
   return (
     <div className="container-admin">
-        <AdminNav></AdminNav>
+      <AdminNav></AdminNav>
 
-        <div className="cars-input-container">
+      <div className="cars-input-container">
+        <table>
+          <thead>
+            <th></th>
+          </thead>
+          <tbody>
 
-            <table>
-            <thead>
-                <th></th>
-            </thead>
-            <tbody>
-                <tr>
-                <td className="row">
-                    <label className="insert-car-label">Car Model</label>{" "}
-                </td>
-                <td className="input-row">
-                    <input className="insert-car-input" type="text" />{" "}
-                </td>
-                </tr>
+            <tr >
+              <td className="row">
+                <label className="insert-car-label">Car Model</label>
+              </td>
+              <td className="input-row">
+                <input className="insert-car-input" type="text" />
+              </td>
+            </tr>
 
-                <tr>
-                <td>
-            
-                    <label className="insert-car-label">Driver Name</label>
-                </td>
-                <td>  <input className="insert-car-input" type="text" /> </td>
-                
-                </tr>
+            <tr>
+              <td>
+                <label className="insert-car-label">Driver Name</label>
+              </td>
+              <td>
+                <input className="insert-car-input" type="text" />
+              </td>
+            </tr>
 
-                <tr>
-                <td>
-                    <label className="insert-car-label">Driver Rating</label>
-                </td>
-                <td> <input className="insert-car-input" type="text" /> </td>
-                
-                </tr>
+            <tr>
+              <td>
+                <label className="insert-car-label">Driver Rating</label>
+              </td>
+              <td>
+                <input className="insert-car-input" type="text" />
+              </td>
+            </tr>
 
-                <tr>
-                <td>
-            
-                    <label className="insert-car-label">car Rating</label>
-                </td>
-                <td>  <input className="insert-car-input" type="text" /> </td>
-                
-                </tr>
+            <tr>
+              <td>
+                <label className="insert-car-label">car Rating</label>
+              </td>
+              <td>
+                <input className="insert-car-input" type="text" />
+              </td>
+            </tr>
 
-                <tr>
-                <td>
-            
-                    <label className="insert-car-label">Seats</label>
-                </td>
-                
-                <td>  <input className="insert-car-input" type="text" /> </td>
-                </tr>
+            <tr>
+              <td>
+                <label className="insert-car-label">Seats</label>
+              </td>
 
-                <tr>
-                <td>
+              <td>
+                <input className="insert-car-input" type="text" />
+              </td>
+            </tr>
 
-                    <label className="insert-car-label">Pricing per km</label>{" "}
-                </td>
+            <tr>
+              <td>
+                <label className="insert-car-label">Pricing per km</label>
+              </td>
+              <td>
+                <input className="insert-car-input" type="text" />
+              </td>
+            </tr>
 
-                <td> <input className="insert-car-input" type="text" /> </td>
-                
-                </tr>
+            <tr>
+              <td>
+                <label className="insert-car-label">Contact Number</label>
+              </td>
 
-                <tr>
-                <td>
-            
-                    <label className="insert-car-label">Contact Number</label>
-                </td>
+              <td>
+                <input className="insert-car-input" type="text" />
+              </td>
+            </tr>
 
-                <td>  <input className="insert-car-input" type="text" /> </td>
-                
-                </tr>
+            <tr>
+              <td>
+                <label className="insert-car-label">Email</label>
+              </td>
 
-                <tr>
-                <td>
-                    
-                    <label className="insert-car-label">Email</label>
-                </td>
-                
-                <td>  <input className="insert-car-input" type="text" /> </td>
-                </tr>
+              <td>
+                <input className="insert-car-input" type="text" />
+              </td>
+            </tr>
 
-                <tr>
-                <td>
-                
-                    <label className="insert-car-label">Image 1</label>
-                </td>
-                <td>  <input className="insert-car-input" type="text" /> </td>
-                
-                </tr>
+            <tr>
+              <td>
+                <label className="insert-car-label">Image 1</label>
+              </td>
+              <td>
+                <input className="insert-car-input" type="text" />
+              </td>
+            </tr>
 
-                <tr>
-                <td>
-                    
-                    <label className="insert-car-label">Image 2</label>
-                </td>
-                <td> <input className="insert-car-input" type="text" /> </td>
-                
-                </tr>
+            <tr>
+              <td>
+                <label className="insert-car-label">Image 2</label>
+              </td>
+              <td>
+                <input className="insert-car-input" type="text" />
+              </td>
+            </tr>
 
-                <tr>
-                <td>
-                
-                    <label className="insert-car-label">Image 3</label>
-                </td>
-                <td> <input className="insert-car-input" type="text" /></td>
-            
-                </tr>
+            <tr>
+              <td>
+                <label className="insert-car-label">Image 3</label>
+              </td>
+              <td>
+                <input className="insert-car-input" type="text" />
+              </td>
+            </tr>
 
-                <tr>
-                <td>
-            
-                    <label className="insert-car-label">Image 4</label>
-                </td>
-                <td>  <input className="insert-car-input" type="text" /> </td>
-                
-                </tr>
+            <tr>
+              <td>
+                <label className="insert-car-label">Image 4</label>
+              </td>
+              <td>
+                <input className="insert-car-input" type="text" />
+              </td>
+            </tr>
+          </tbody>
+        </table>
 
-            </tbody>
-            </table>
-        </div>
+      </div>
 
     </div>
-
-  )
+  );
 }
 
-export default Insertcars
+export default Insertcars;

e0809056f5435d95d2a68b285699f17090d8fcf2
Amit-Bongale <amitbongale14@gmail.com>
2024-01-13 14:48:51 +0530

    added cahtbot in index file

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -14,61 +14,9 @@ function Home(){
       window.scrollTo(0,0)
     },[])
 
-    // let botpressScript;
-    // let configScript;
-
-    // useEffect(() => {
-    //     const loadBotpress = async () => {
-    //       try {
-    //         // Create script element for Botpress webchat library
-    //         botpressScript = document.createElement('script');
-    //         botpressScript.src = 'https://cdn.botpress.cloud/webchat/v1/inject.js';
-    //         document.body.appendChild(botpressScript);
-    
-    //         // Wait for the library script to load before proceeding
-    //         await new Promise((resolve, reject) => {
-    //           botpressScript.onload = resolve;
-    //           botpressScript.onerror = reject;
-    //         });
-    
-    //         // Create script element for Botpress webchat configuration
-    //         configScript = document.createElement('script');
-    //         configScript.src = 'https://mediafiles.botpress.cloud/b5a8718c-3f2f-4c41-b6a0-b6b1ce01ddf5/webchat/config.js';
-    //         configScript.defer = true;
-    //         document.body.appendChild(configScript);
-    
-    //         // Wait for the configuration script to load before proceeding
-    //         await new Promise((resolve, reject) => {
-    //           configScript.onload = resolve;
-    //           configScript.onerror = reject;
-    //         });
-    //       } catch (error) {
-    //         console.error('Error loading Botpress:', error);
-    //         // Handle the error (e.g., show an error message)
-    //       }
-    //     };
-    
-    //     // Call the async function to load Botpress scripts
-    //     loadBotpress();
-    
-    //     // Cleanup: Remove script elements when component unmounts
-    //     return () => {
-    //       if (botpressScript && botpressScript.parentNode) {
-    //         botpressScript.parentNode.removeChild(botpressScript);
-    //       }
-    //       if (configScript && configScript.parentNode) {
-    //         configScript.parentNode.removeChild(configScript);
-    //       }
-    //     };
-        
-    //   }, []); // Empty dependency array ensures that the effect runs only once on mount
-    
-
-
 
     return(
         <div>
-            
             <Nav></Nav>
             <Herosection></Herosection>
             <FeaturedCars></FeaturedCars>
